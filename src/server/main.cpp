5649607960aa348d302662e4f985ace61715e6b3
Josh Bialkowski <jbialk@mit.edu>
2013-02-08 13:38:31 -0500

    added server tcp demo app

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -25,13 +25,103 @@
  */
 
 
-int main()
+#include <stdio.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <iostream>
+
+#define MAXPENDING 5    /* Max connection requests */
+#define BUFFSIZE 32
+
+void HandleClient(int sock)
 {
-    return 0;
+    char buffer[BUFFSIZE];
+    int received = -1;
+    /* Receive message */
+    if ((received = recv(sock, buffer, BUFFSIZE, 0)) < 0)
+    {
+        std::cerr << "Failed to receive initial bytes from client\n";
+        exit(1);
+    }
+    /* Send bytes and check for more incoming data in loop */
+    while (received > 0)
+    {
+        /* Send back received data */
+        if (send(sock, buffer, received, 0) != received)
+        {
+            std::cerr << "Failed to send bytes to client\n";
+            exit(1);
+        }
+
+        /* Check for more data */
+        if ((received = recv(sock, buffer, BUFFSIZE, 0)) < 0)
+        {
+            std::cerr << "Failed to receive additional bytes from client\n";
+            exit(1);
+        }
+    }
+    close(sock);
 }
 
+int main(int argc, char** argv)
+{
+    int serversock, clientsock;
+    struct sockaddr_in echoserver, echoclient;
 
+    if (argc != 2)
+    {
+        std::cerr << "USAGE: echoserver <port>\n";
+        exit(1);
+    }
 
+    /* Create the TCP socket */
+    if ((serversock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
+    {
+        std::cerr << "Failed to create socket\n";
+        return 1;
+    }
+    /* Construct the server sockaddr_in structure */
+    memset(&echoserver, 0, sizeof(echoserver));       /* Clear struct */
+    echoserver.sin_family = AF_INET;                  /* Internet/IP */
+    echoserver.sin_addr.s_addr = htonl(INADDR_ANY);   /* Incoming addr */
+    echoserver.sin_port = htons(atoi(argv[1]));       /* server port */
+
+    /* Bind the server socket */
+    if (bind(serversock, (struct sockaddr *) &echoserver,
+    sizeof(echoserver)) < 0)
+    {
+        std::cerr << "Failed to bind the server socket\n";
+        return 1;
+    }
+    /* Listen on the server socket */
+    if (listen(serversock, MAXPENDING) < 0)
+    {
+        std::cerr << "Failed to listen on server socket\n";
+    }
+
+    /* Run until cancelled */
+    while (1)
+    {
+        unsigned int clientlen = sizeof(echoclient);
+        /* Wait for client connection */
+        clientsock = accept(
+                serversock,
+                (struct sockaddr *) &echoclient,
+                &clientlen );
+        if (clientsock < 0)
+        {
+            std::cerr << "Failed to accept client connection\n";
+            return 1;
+        }
+        fprintf(stdout, "Client connected: %s\n",
+                    inet_ntoa(echoclient.sin_addr));
+        HandleClient(clientsock);
+    }
+}
 
 
 
