2af7ce62c25069d146f7f38a61e595e797d4660f
Josh Bialkowski <jbialk@mit.edu>
2013-04-30 15:00:56 -0400

    split up SetClientSocket

---
+++ src/clui/commands/SetClientSocket.cpp
@@ -0,0 +1,82 @@
+
+#include "connection.h"
+#include "global.h"
+#include "FileDescriptor.h"
+#include "ReferenceCounted.h"
+#include "ExceptionStream.h"
+#include "SetClientSocket.h"
+
+namespace   openbook {
+namespace filesystem {
+namespace       clui {
+
+
+const std::string SetClientSocket::COMMAND      = "clientSocket";
+const std::string SetClientSocket::DESCRIPTION  = "the socket to listen on for peers";
+
+
+SetClientSocket::SetClientSocket(TCLAP::CmdLine& cmd):
+    Options(cmd),
+    addressFamily(
+        "address family",
+        "AF_INET, AF_INET6, AF_UNIX",
+        true,
+        0,
+        "address family",
+        cmd),
+    node( "address",
+                "remote network interface to use",
+                true,
+                "any",
+                "host",
+                cmd
+                )
+    {}
+	
+
+void SetClientSocket::go(){
+    FdPtr_t sockfd = connectToClient(*this);    //< create a connection
+        Marshall marshall;        //< create a marshaller
+        marshall.setFd(*sockfd);  //< tell the marshaller the socket to use
+        handshake(marshall);      //< perform handshake protocol
+
+        // send the message
+        messages::SetClientSocket* msg =
+            new messages::SetClientSocket();
+        // fill the message
+        msg->set_addressfamily(addressFamily.getValue());
+    msg->set_node(node.getValue());
+
+        // send the message to the backend
+        marshall.writeMsg(msg);
+
+        // wait for the reply
+        RefPtr<AutoMessage> reply = marshall.read();
+
+        // if the backend replied with a message we weren't expecting then
+        // print an error
+        if( reply->type != MSG_UI_REPLY )
+        {
+                std::cerr << "Unexpected reply of type: "
+                  << messageIdToString( reply->type )
+                  << "\n";
+        }
+        // otherwise print the result of the operation
+        else
+        {
+        messages::UserInterfaceReply* msg =
+                static_cast<messages::UserInterfaceReply*>(reply->msg);
+           std::cout << "Server reply: "
+                  << "\n    ok? : " << (msg->ok() ? "YES" : "NO")
+                  << "\nmessage : " << msg->msg()
+                  << "\n";
+        }
+}
+
+
+
+
+}
+}
+}
+
--- src/clui/commands/SetClientSocket.h
+++ src/clui/commands/SetClientSocket.h
@@ -14,73 +14,12 @@ class SetClientSocket:
 	TCLAP::UnlabeledValueArg<std::string> node;
 	
 	public:
-	static const std::string COMMAND;
-	static const std::string DESCRIPTION;
+        static const std::string COMMAND;
+        static const std::string DESCRIPTION;
 
-	SetClientSocket(TCLAP::CmdLine& cmd):
-	Options(cmd),
-	addressFamily(
-		"address family",
-		"AF_INET, AF_INET6, AF_UNIX",
-		true,
-		0,
-		"address family",
-		cmd),
-	node( "address",
-                "remote network interface to use",
-                true,
-                "any",
-                "host",
-                cmd
-                )
-	
-		
-	{}
-	
-
-	void go(){
-		FdPtr_t sockfd = connectToClient(*this);    //< create a connection
-        	Marshall marshall;        //< create a marshaller
-       		marshall.setFd(*sockfd);  //< tell the marshaller the socket to use
-        	handshake(marshall);      //< perform handshake protocol
-
-        	// send the message
-        	messages::SetClientSocket* msg =
-                new messages::SetClientSocket();
-        	// fill the message
-        	msg->set_addressfamily(addressFamily.getValue());
-		msg->set_node(node.getValue());
-
-       		// send the message to the backend
-       		marshall.writeMsg(msg);
-
-        	// wait for the reply
-        	RefPtr<AutoMessage> reply = marshall.read();
-
-        	// if the backend replied with a message we weren't expecting then
-        	// print an error
-        	if( reply->type != MSG_UI_REPLY )
-        	{
-            		std::cerr << "Unexpected reply of type: "
-                      << messageIdToString( reply->type )
-                      << "\n";
-        	}
-        	// otherwise print the result of the operation
-        	else
-        	{
-         	messages::UserInterfaceReply* msg =
-                    static_cast<messages::UserInterfaceReply*>(reply->msg);
-         	   std::cout << "Server reply: "
-                      << "\n    ok? : " << (msg->ok() ? "YES" : "NO")
-                      << "\nmessage : " << msg->msg()
-                      << "\n";
-	        }
-	}
+        SetClientSocket(TCLAP::CmdLine& cmd);
+        void go();
 };
-
-
-const std::string SetClientSocket::COMMAND      = "clientSocket";
-const std::string SetClientSocket::DESCRIPTION  = "the socket to listen on for peers";
 
 
 }
